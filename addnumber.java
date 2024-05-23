55a3be3d55fb8cdacd4702b7c640504cd2c487e5
saipruthiraj <145189719+saipruthiraj@users.noreply.github.com>
2024-05-23 05:45:03 +0000

    addition

--- addnumber.java
+++ addnumber.java
@@ -1,3 +1,4 @@
+
 class add{
     int addition(int num1,int num2){
         int r= num1+num2;
@@ -6,7 +7,7 @@ class add{
 }
 class addnumber{
     public static void main(String args[]){
-        add a=new add();
-        System.out.println(a.addition(1,2));
+            add a=new add();
+        System.out.println(a.addition(23,43));
     }
 }
