6adac4cb4ad4bcb688b74b3f77a3914000fcbce9
saipruthiraj <145189719+saipruthiraj@users.noreply.github.com>
2024-05-24 05:22:10 +0000

    Playing with string using String buffer class

---
+++ PlayingString.java
@@ -0,0 +1,9 @@
+class PlayingString{
+    public static void main(String args[]){
+        StringBuffer sb=new StringBuffer("SAI");
+        System.out.println(sb.charAt(0));
+        System.out.println(sb);
+        
+
+    }
+}
